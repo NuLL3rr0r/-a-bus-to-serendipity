3fed75c18635c25f39cfca607a65d76b37a287b2
Mohammad S. Babaei <info@babaei.net>
2015-11-26 03:20:30 +0330

    Fixed builds for other platforms to find AppDelegate.hpp

--- proj.android-studio/app/jni/hellocpp/main.cpp
+++ proj.android-studio/app/jni/hellocpp/main.cpp
@@ -1,4 +1,4 @@
-#include "AppDelegate.h"
+#include "AppDelegate.hpp"
 #include "cocos2d.h"
 #include "platform/android/jni/JniHelper.h"
 #include <jni.h>
--- proj.ios_mac/mac/main.cpp
+++ proj.ios_mac/mac/main.cpp
@@ -22,7 +22,7 @@
  THE SOFTWARE.
  ****************************************************************************/
 
-#include "AppDelegate.h"
+#include "AppDelegate.hpp"
 #include "cocos2d.h"
 
 USING_NS_CC;

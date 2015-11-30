fe84f555e9634b4cabfce0f234eccc0f4f1c2a61
Mohammad S. Babaei <info@babaei.net>
2015-11-30 04:24:58 +0330

    Animated main menu

--- Classes/MainMenuScene.cpp
+++ Classes/MainMenuScene.cpp
@@ -67,14 +67,54 @@ MainMenuScene::Impl::~Impl()
 
 void MainMenuScene::Impl::createTitle()
 {
-	auto vgap = VisibleRect::height() * 0.25;
+	auto vGap = VisibleRect::height() * 0.25f;
 
 	auto titleLabel = Label::createWithTTF("A Bus to Serendipity", "fonts/Marker Felt.ttf", 24);
-	titleLabel->setPosition(Point(VisibleRect::center().x, VisibleRect::center().y + vgap));
+	titleLabel->setPosition(Point(VisibleRect::center().x, VisibleRect::center().y + vGap));
 	m_parent->addChild(titleLabel);
 }
 
 void MainMenuScene::Impl::createMenu()
 {
+	auto vGap = VisibleRect::height() * 0.10f;
+	float vOffset = 40.0f;
 
+	MenuItemFont::setFontName("fonts/Marker Felt.ttf");
+	MenuItemFont::setFontSize(20);
+
+	auto newGameItem = MenuItemFont::create("New Game", [&](Ref *sender) {
+	});
+
+	auto creditsItem = MenuItemFont::create("Credits", [&](Ref *sender) {
+	});
+
+	auto exitItem = MenuItemFont::create("Exit", [&](Ref *sender) {
+		Director::getInstance()->end();
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
+		exit(EXIT_SUCCESS);
+#endif
+	});
+
+	auto menu = Menu::create(newGameItem, creditsItem, exitItem, nullptr);
+	menu->setPosition(Point::ZERO);
+
+	float nextItemOffset = 0.0f;
+	int i = 0;
+	for (const auto& child : menu->getChildren()) {
+		child->setPosition(Point(VisibleRect::center().x, VisibleRect::center().y + vGap - nextItemOffset));
+		nextItemOffset += vOffset;
+
+		auto dstPoint = child->getPosition();
+		auto offset = VisibleRect::center().x + 50.0f;
+		if (i % 2 == 0)
+			offset = -offset;
+
+		child->setPosition(Vec2(dstPoint.x + offset, dstPoint.y));
+		child->runAction(
+			EaseElasticOut::create(MoveTo::create(2.0f, Vec2(dstPoint.x, dstPoint.y)), 0.35f)
+			);
+		i++;
+	}
+
+	m_parent->addChild(menu);
 }
